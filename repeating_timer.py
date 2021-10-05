f56ce0e5087c71878c92f08d992955af332d04b3
Quin Marilyn <quin.marilyn05@gmail.com>
2021-10-05 12:27:04 -0600

    The callback can now take parameters.

--- repeating_timer.py
+++ repeating_timer.py
@@ -1,16 +1,18 @@
 import threading
 
 class RepeatingTimer(threading.Thread):
-	def __init__(self, interval, function):
+	def __init__(self, interval, function, *args, **kwargs):
 		threading.Thread.__init__(self)
 		self.interval = interval
 		self.function = function
+		self.args = args
+		self.kwargs = kwargs
 		self.stopped = threading.Event()
 
 	def run(self):
 		while not self.stopped.wait(self.interval):
 			try:
-				self.function()
+				self.function(*self.args, **self.kwargs)
 			except:
 				pass
 
