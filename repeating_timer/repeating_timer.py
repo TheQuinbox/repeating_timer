f85393081369f9ef9a1ebfd650454e319d6a1cbb
Quin Marilyn <quin.marilyn05@gmail.com>
2021-10-26 16:14:17 -0600

    Formatting.

--- repeating_timer/repeating_timer.py
+++ repeating_timer/repeating_timer.py
@@ -1,20 +1,20 @@
 import threading
 
 class RepeatingTimer(threading.Thread):
- def __init__(self, interval, function, *args, **kwargs):
-  threading.Thread.__init__(self)
-  self.interval = interval
-  self.function = function
-  self.args = args
-  self.kwargs = kwargs
-  self.stopped = threading.Event()
+	def __init__(self, interval, function, *args, **kwargs):
+		threading.Thread.__init__(self)
+		self.interval = interval
+		self.function = function
+		self.args = args
+		self.kwargs = kwargs
+		self.stopped = threading.Event()
 
- def run(self):
-  while not self.stopped.wait(self.interval):
-   try:
-    self.function(*self.args, **self.kwargs)
-   except Exception as e:
-    raise e
+	def run(self):
+		while not self.stopped.wait(self.interval):
+			try:
+				self.function(*self.args, **self.kwargs)
+			except Exception as e:
+				raise e
 
- def stop(self):
-  self.stopped.set()
+	def stop(self):
+		self.stopped.set()
--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
- name="repeating_timer",
- version=0.2,
- author="Quin Marilyn",
- author_email="quin.marilyn05@gmail.com",
- description="Run a function over and over in a thread",
- long_description=open("readme.md", "r").read(),
- long_description_content_type="text/markdown",
- url="http://github.com/TheQuinbox/repeating_timer",
- packages=find_packages(),
- classifiers=[
-  "Programming Language :: Python :: 3",
-  "License :: OSI Approved :: MIT License",
-  "Operating System :: OS Independent",
-  "Development Status :: 5 - Production/Stable",
-  "Topic :: Utilities",
- ],
- python_requires=">=3",
+	name="repeating_timer",
+	version=0.2,
+	author="Quin Marilyn",
+	author_email="quin.marilyn05@gmail.com",
+	description="Run a function over and over in a thread",
+	long_description=open("readme.md", "r").read(),
+	long_description_content_type="text/markdown",
+	url="http://github.com/TheQuinbox/repeating_timer",
+	packages=find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		"Development Status :: 5 - Production/Stable",
+		"Topic :: Utilities",
+	],
+	python_requires=">=3",
 )
